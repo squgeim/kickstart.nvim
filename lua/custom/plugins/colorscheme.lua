482512be5bdc9074b8252d653db07d45c6f93a91
Shreya Dahal <sdahal@owlpracticesuite.com>
2024-11-29 09:52:05 -0700

    vim tmux nav

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,18 +1,22 @@
 vim.defer_fn(function()
-  vim.o.background = 'light'
+  vim.o.termguicolors = true
+  vim.o.background = 'dark'
   -- vim.cmd.colorscheme 'modus'
   -- vim.cmd.colorscheme 'tokyonight'
-  vim.cmd.colorscheme 'vscode'
+  -- vim.cmd.colorscheme 'vscode'
   -- vim.cmd.colorscheme 'dawnfox'
   -- vim.cmd.colorscheme 'lunaperche'
+  -- vim.cmd.colorscheme 'github_light_tritanopia'
+  -- vim.cmd.colorscheme 'modus'
+  -- vim.cmd.colorscheme 'rose-pine'
+  vim.cmd.colorscheme 'adwaita'
+  -- vim.cmd.colorscheme 'sorbet'
 end, 0)
 
 return {
   'morhetz/gruvbox',
   'nlknguyen/papercolor-theme',
-  'cormacrelf/vim-colors-github',
   'kristijanhusak/vim-hybrid-material',
-  'icymind/neosolarized',
   'humanoid-colors/vim-humanoid-colorscheme',
   'nordtheme/vim',
   'EdenEast/nightfox.nvim',
@@ -20,11 +24,18 @@ return {
   'dgraham/xcode-low-key-vim',
   'sainnhe/sonokai',
   'miikanissi/modus-themes.nvim',
-  {
-    'lalitmee/cobalt2.nvim',
-    dependencies = { 'tjdevries/colorbuddy.nvim', tag = 'v1.0.0' },
-    init = function()
-      -- require("colorbuddy").colorscheme("cobalt2")
-    end,
-  },
+  -- {
+  --   'lalitmee/cobalt2.nvim',
+  --   dependencies = { 'tjdevries/colorbuddy.nvim', tag = 'v1.0.0' },
+  --   init = function()
+  --     -- require("colorbuddy").colorscheme("cobalt2")
+  --   end,
+  -- },
+  { 'projekt0n/github-nvim-theme', name = 'github-theme' },
+  'rose-pine/neovim',
+  'Mofiqul/adwaita.nvim',
+  'maxmx03/solarized.nvim',
+  'daschw/leaf.nvim',
+  'cdmill/neomodern.nvim',
+  'e-q/okcolors.nvim',
 }
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -99,4 +99,21 @@ return {
       vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
     end,
   },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
 }
